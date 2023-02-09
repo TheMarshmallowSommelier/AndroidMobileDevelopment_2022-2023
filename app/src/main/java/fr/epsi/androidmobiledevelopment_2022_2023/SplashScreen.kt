b35c75135cd3526e9e8767be033b307445f20111
Joey <joey.frimin@soprabanking.com>
2023-02-09 17:37:00 +0100

    [FIX]name MainActivity to HomeActivity

--- app/src/main/java/fr/epsi/androidmobiledevelopment_2022_2023/SplashScreen.kt
+++ app/src/main/java/fr/epsi/androidmobiledevelopment_2022_2023/SplashScreen.kt
@@ -11,7 +11,7 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
-            val newIntent=Intent(application,MainActivity::class.java)
+            val newIntent=Intent(application,HomeActivity::class.java)
             startActivity(newIntent)
         },2000)
     }
